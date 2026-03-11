f0066709af58890a648f68e858d5049cfb155ea7
jasper123234 <jasper.cronin@gmail.com>
2026-03-11 11:35:03 -0700

    shoot

--- MesoAmerica/src/components/CivilizationProfile.jsx
+++ MesoAmerica/src/components/CivilizationProfile.jsx
@@ -1,6 +1,6 @@
 function CivilizationProfile({ civilizationName,Bio,profileImg}) {
   return 
-  <div>
+  <div class="text-{green}-{600} bg-{brown}-{600} p-6 rounded-lg shadow-md text-center italic">
   <h1>{civilizationName} Profile Page</h1>;
   <p>{Bio}</p>
   <img src={profileImg}/>
