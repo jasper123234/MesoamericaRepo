491b6d665e9134a2cc702fd3b4451a175ebc3f9b
Tajin <tajin088@gmail.com>
2026-03-18 10:51:57 -0700

    comsuk

--- MesoAmerica/src/Pages/Back.jsx
+++
@@ -1,9 +0,0 @@
-function BackButton(){
-  return(
-    <div class = "w-20 bg-yellow-500 border border-yellow-800 mt-3 shadow-md rounded-2xl hover:shadow-xl">
-      <h1 class = "text-center text-2xl"> Back </h1>
-    </div>
-  )
-}
-
-export default BackButton;
---
+++ MesoAmerica/src/Pages/BackButton.jsx
@@ -0,0 +1,9 @@
+function BackButton(){
+  return(
+    <div class = "w-20  border border-yellow-800 mt-3 shadow-md rounded-2xl hover:shadow-xl text-yellow-900">
+      <h1 class = "text-center text-2xl border-4"> Back </h1>
+    </div>
+  )
+}
+
+export default BackButton;
