a92d207001cfdf548cedabb1b9ef578e63cf0337
Tajin <tajin088@gmail.com>
2026-03-20 11:18:42 -0700

    dfhjsegfuweugfq3oi/hlfihl3

---
+++ MesoAmerica/src/Pages/Olmec.jsx
@@ -0,0 +1,22 @@
+function App(){
+  return(
+    <div>
+      <Header
+        title = 'Olmec Civilization'/>
+      <BackButton/> 
+        <Module
+          icon = "dante.jpg"
+          x = "top-60"
+          y = "left-15"/>
+        <Module
+          icon = "dante.jpg"
+          x = "top-90"
+          y = "right-15"/>  
+        <Module
+          icon = "dante.jpg"
+          x = "bottom-40"
+          y = "right-30"/> 
+    </div>
+  );
+}
+export default App;
