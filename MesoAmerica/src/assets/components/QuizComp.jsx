244634a323c52e8e6a815e7fa81c80b67fd365d7
jasper123234 <jasper.cronin@gmail.com>
2026-03-25 11:16:33 -0700

    quizcomp

---
+++ MesoAmerica/src/assets/components/QuizComp.jsx
@@ -0,0 +1,16 @@
+function QuizComp({question, wAns1, wAns2, rAns}){
+    return(
+       <form>
+      <h2>{question}</h2>
+      <input type="radio" name="answer" value={wAns1} /> {wAns1} <br />
+      <input type="radio" name="answer" value={wAns2} /> {wAns2} <br />
+      <input type="radio" name="answer" value={rAns} /> {rAns} <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+  
+ 
+  }
+  
+  export default QuizComp;
+  
