b25de5c42d57bcee4b326bba042d2be1d60af589
EzraZC <ezrazeliecampbell@gmail.com>
2026-03-27 11:25:38 -0700

    now

--- MesoAmerica/src/assets/components/Login.jsx
+++ MesoAmerica/src/assets/components/Login.jsx
@@ -1,4 +1,4 @@
-function Login(){
+function login(){
   return(
     <form>
       <input type = "text" value = "" placeholder = "Username" id = "username" class = "text-2xl text-yellow-900 text-center b border border-yellow-900 c p-1 mt-5  border-4 justify-center align-items: center;"/>
@@ -14,4 +14,4 @@ function Login(){
   )
 
 }
-export default Login;
+export default login;
