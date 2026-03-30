4aabc003395f83140d1650fcc6287ed26593cc3c
jasper123234 <jasper.cronin@gmail.com>
2026-03-30 11:15:53 -0700

    bull

--- MesoAmerica/src/App.jsx
+++ MesoAmerica/src/App.jsx
@@ -1,22 +1,24 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./assets/Pages/Home";
-import About from "./assets/Pages/About";
-import Profile from "./assets/Pages/CivilizationProfile";
-import CivilizationProfile from "./assets/Pages/CivilizationProfile";
+// import Home from "./assets/Pages/Home";
+// import About from "./assets/Pages/About";
+// import Profile from "./assets/Pages/CivilizationProfile";
+import Mayan from "./assets/Pages/Mayan";
+import Olmec from "./assets/Pages/Olmec";
+import Aztec from "./assets/Pages/Aztec";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/CivilizationProfile">Civilization Profile</Link>
+        <Link to="/Aztec">Aztec Profile</Link>
+        <Link to="/Olmec">Olmec Profile</Link>
+        <Link to="/Mayan">Mayan Profile</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/CivilizationProfile" element={<CivilizationProfile />} />
+        <Route path="/Aztec" element={<Aztec />} /> 
+        <Route path="/Olmec" element={<Olmec />} /> 
+        <Route path="/Mayan" element={<Mayan />} />
       </Routes>
     </BrowserRouter>
   );
