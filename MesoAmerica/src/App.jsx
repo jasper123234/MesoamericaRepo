e44318da02342b8a83665424b1aac2c4e0606b11
jasper123234 <jasper.cronin@gmail.com>
2026-04-01 10:59:46 -0700

    tuff

--- MesoAmerica/src/App.jsx
+++ MesoAmerica/src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
 import Mayan from "./assets/Pages/Mayan";
 import Olmec from "./assets/Pages/Olmec";
 import Aztec from "./assets/Pages/Aztec";
+import ProfilePage from "./assets/Pages/ProfilePage";
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/Aztec">Aztec Profile</Link>
+        <Link to="/ProfilePage">Profile Page</Link>
         <Link to="/Olmec">Olmec Profile</Link>
         <Link to="/Mayan">Mayan Profile</Link>
       </nav>
 
       <Routes>
         <Route path="/Aztec" element={<Aztec />} /> 
+        <Route path="/ProfilePage" element={<ProfilePage />} /> 
         <Route path="/Olmec" element={<Olmec />} /> 
         <Route path="/Mayan" element={<Mayan />} />
       </Routes>
--- MesoAmerica/src/assets/components/QuizComp.jsx
+++ MesoAmerica/src/assets/components/QuizComp.jsx
@@ -1,10 +1,10 @@
-function QuizComp({question, wAns1, wAns2, rAns}){
+function QuizComp({ question, wAns1, wAns2, rAns}){
     return(
        <form>
       <h2>{question}</h2>
       <input type="radio" name="answer" value={wAns1} /> {wAns1} <br />
       <input type="radio" name="answer" value={wAns2} /> {wAns2} <br />
-      <input type="radio" name="answer" value={rAns} /> {rAns} <br />
+      <input type="radio" name="answer" value={rAns} /> {rAns} <br/>
       <button type="submit">Submit</button>
     </form>
   );
