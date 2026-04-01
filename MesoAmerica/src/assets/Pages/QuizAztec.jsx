45c8272f5796585ffe711f7b47865a791e9ec1aa
EzraZC <ezrazeliecampbell@gmail.com>
2026-04-01 11:35:55 -0700

    Imbouttapushit

---
+++ MesoAmerica/src/assets/Pages/QuizAztec.jsx
@@ -0,0 +1,28 @@
+import QuizComp from "./components/QuizComp.jsx"
+import Header from "./components/Header.jsx"
+
+
+function QuizAztec(){
+  return(
+    <div>
+      <Header
+        title = "Aztec Quiz #1"
+      />
+      <QuizComp
+        question = "What is the capital city in the Aztec Empire?"
+        wAns1 = "Acapulco"
+        wAns2 = "Zacatecas"
+        rAns = "Tenochtitlan"
+      />
+      <QuizComp
+        question = "What is the name of their sun God"
+        wAns1 = "Tezcatlipoca"
+        wAns2 = "Quetzalcoatl"
+        rAns = "Huitzilopochtli"
+      />
+      
+      
+    </div>
+  );
+}
+export default QuizAztec;
---
+++ MesoAmerica/src/assets/Pages/QuizMayan.jsx
@@ -0,0 +1,28 @@
+import QuizComp from "./components/QuizComp.jsx"
+import Header from "./components/Header.jsx"
+
+
+function QuizMayan(){
+  return(
+    <div>
+      <Header
+        title = "Mayan Quiz #1"
+      />
+      <QuizComp
+        question = "What was the most important crop in Maya daily life?"
+        wAns1 = "Wheat"
+        wAns2 = "Rice"
+        rAns = "Corn"
+      />
+      <QuizComp
+        question = "How did the Maya most commonly consume chocolate?"
+        wAns1 = "As solid candy bars"
+        wAns2 = "Mixed with cold milk"
+        rAns = "Ground into a drink with water and spices"
+      />
+      
+      
+    </div>
+  );
+}
+export default QuizMayan;
