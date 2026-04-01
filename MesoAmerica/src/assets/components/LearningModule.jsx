c6f2d7a9594cb7096debb4bcae048d6a47571b5e
jasper123234 <jasper.cronin@gmail.com>
2026-04-01 11:11:00 -0700

    learning mod

--- MesoAmerica/src/assets/components/LearningModule.jsx
+++ MesoAmerica/src/assets/components/LearningModule.jsx
@@ -1,9 +1,9 @@
-function LearningModule({text1,text2,img1,img2}){
+function LearningModule({title,info,img1}){
   return(
     <div>
       <div class = "b border border-yellow-900 c p-3 mt-3  border-4">
         <h1 class  = "text-xl text-yellow-900 text-center">
-          {text1}
+          {title}
         </h1>
         <img
           src={img1}
@@ -13,15 +13,10 @@ function LearningModule({text1,text2,img1,img2}){
         />
       </div>
       <div class = "b border border-yellow-900 c p-3 mt-3  border-4">
-        <h1 class  = "text-xl text-yellow-900 text-center">
-          {text2}
-        </h1>
-        <img
-          src={img2}
-          alt="img1"
-          width="200"
-          height="200"
-        />
+        <p class = "text-lg text-yellow-900 text-center">
+          {info}
+        </p>
+       
       </div>
     </div>
   )
