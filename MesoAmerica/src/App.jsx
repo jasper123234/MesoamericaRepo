5e42ee6c3cddbd5ced63069210c72a7589f75e70
EzraZC <ezrazeliecampbell@gmail.com>
2026-04-01 10:54:34 -0700

    stiga

--- MesoAmerica/src/App.jsx
+++ MesoAmerica/src/App.jsx
@@ -1,29 +1,24 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Friends from "./assets/Pages/Friends";
-import LearningModule1 from "./assets/Pages/LearningModule1";
-import Login from "./assets/Pages/Login";
+
+import Mayan from "./assets/Pages/Mayan";
 import Olmec from "./assets/Pages/Olmec";
-import ProfilePage from "./assets/Pages/ProfilePage";
+import Aztec from "./assets/Pages/Aztec";
 
 function App() {
   return (
- <BrowserRouter>
-  <nav>
-    <Link to="/">Friends</Link> |{" "}
-    <Link to="/learning">Learning</Link> |{" "}
-    <Link to="/login">Login</Link> |{" "}
-    <Link to="/olmec">Olmec</Link> |{" "}
-    <Link to="/profile">Profile</Link>
-  </nav>
+    <BrowserRouter>
+      <nav>
+        <Link to="/Aztec">Aztec Profile</Link>
+        <Link to="/Olmec">Olmec Profile</Link>
+        <Link to="/Mayan">Mayan Profile</Link>
+      </nav>
 
-  <Routes>
-    <Route path="/" element={<Friends />} />
-    <Route path="/learning" element={<LearningModule1 />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/olmec" element={<Olmec />} />
-    <Route path="/profile" element={<ProfilePage />} />
-  </Routes>
-</BrowserRouter>
+      <Routes>
+        <Route path="/Aztec" element={<Aztec />} /> 
+        <Route path="/Olmec" element={<Olmec />} /> 
+        <Route path="/Mayan" element={<Mayan />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
