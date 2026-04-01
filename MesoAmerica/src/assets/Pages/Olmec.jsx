236679313fe3a8c8e92850c7f39b167a80137c92
Tajin <tajin088@gmail.com>
2026-04-01 11:36:22 -0700

    yuh

--- MesoAmerica/src/assets/Pages/Olmec.jsx
+++ MesoAmerica/src/assets/Pages/Olmec.jsx
@@ -9,14 +9,17 @@ function Olmec(){
         title = 'Olmec Civilization'/>
       <BackButton/>   
         <Module
+          link = "./OlmecLearningModule.jsx"
           icon = "dante.jpg"
           x = "top-60"
           y = "left-15"/>
         <Module
+         link = "https://2048cupcakes.net/"
           icon = "dante.jpg"
           x = "top-90"
           y = "right-15"/>  
         <Module
+         link = "https://2048cupcakes.net/"
           icon = "dante.jpg"
           x = "top-120"
           y = "right-30"/> 
