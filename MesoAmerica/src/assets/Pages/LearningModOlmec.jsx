0c277072da3e992317e9095281c63edaaf8d8fb6
EzraZC <ezrazeliecampbell@gmail.com>
2026-04-24 10:59:07 -0700

    leo hittimg the uuuder

---
+++ MesoAmerica/src/assets/Pages/LearningModOlmec.jsx
@@ -0,0 +1,20 @@
+import Header from "../components/Header.jsx"
+import LearningModule from "../components/LearningModule.jsx"
+
+
+function LearningModOlmec(){
+  return(
+    <div>
+      <Header
+        title = "Olmec Learning Module #2"
+      />
+      <LearningModule
+        title = "The Olmec Civilization"
+        info = "The Olmec carved huge stone heads weighing up to 50 tons, Each one has a different face, showing real rulers"
+        img1 = "dante.jpg"
+        
+      />
+    </div>
+  );
+}
+export default LearningModOlmec;
