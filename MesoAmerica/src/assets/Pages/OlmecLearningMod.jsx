a5b38620f76dc018450ba43d21a78fa0869a6569
Tajin <tajin088@gmail.com>
2026-04-27 12:00:07 -0700

    olmec

--- MesoAmerica/src/assets/Pages/OlmecLearningMod.jsx
+++ MesoAmerica/src/assets/Pages/OlmecLearningMod.jsx
@@ -13,20 +13,17 @@ function OlmecLearningMod(){
         info = "Location: Lived in the southern part of modern day Mexico, along the gulf coast. Time Period: 1500 BCE - 1200 BCE"
         
       />
-       <LearningModule
-        title = "Fun Facts"
+
+      <LearningModule
+        title = "Olmec Heads"
         info = "The Olmec carved huge stone heads weighing up to 50 tons, Each one has a different face, showing real rulers"
-        
+        img1 = "dante.jpg"
       />
-       <LearningModule
-        title = "Agriculutral Practices"
-        info = "Used slash-and-burn farming to clear land Grew crops together (maize, beans, squash) Built raised fields and canals for water control Farmed based on seasons Maize considered sacred crop"
-        
-      />
-       <LearningModule
-        title = "Social Structure"
-        info = "Elites: Rulers, priests, and nobles who held power and wealth Commoners: Farmers, artisans, and laborers who made up the majority of the population Slaves: Captured individuals used for labor and sacrifice"
-        
+
+      <LearningModule
+        title = "Important Cities"
+        info = "San Lorenzo - First Major City, Known for huge stone heads. La Venta - Main city after San Lorenzo, first Pyramid in Mesoamerica. Tres Zapotes - One of the last major cities, lasting longer than most cities."
+        img1 = "dante.jpg"
       />
     </div>
   );
