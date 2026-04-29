95e718cb52a6def2c341192c811c84c2272529b7
jasper123234 <jasper.cronin@gmail.com>
2026-04-29 11:34:52 -0700

    bang

--- MesoAmerica/src/assets/Pages/QuizMayan.jsx
+++ MesoAmerica/src/assets/Pages/QuizMayan.jsx
@@ -1,8 +1,10 @@
 import QuizComp from "../components/QuizComp.jsx"
 import Header from "../components/Header.jsx"
+import { useState } from "react";
 
 
 function QuizMayan(){
+  const [score, setScore] = useState(0);
   return(
         <div className="min-h-screen w-full bg-gradient-to-b from-green-200 via-green-400 to-green-700">
       <Header
@@ -13,17 +15,22 @@ function QuizMayan(){
         wAns1a = "Wheat"
         wAns1b = "Rice"
         rAns1 = "Corn"
+        
         question2 = "what is the capital of Mexico?"
         wAns2a = "Acapulco"
         wAns2b = "Tenochtitlan"
         rAns2 = "Mexico City"
+        
         question3 = "filler question?"
         wAns3a = "filler answer 1"
         wAns3b = "filler answer 2"
         rAns3 = "filler answer 3"
+        
+        score={score}
+        setScore={setScore}
       />
       
-      
+      {score !== 0 && <h2>Your score is: {score}/3</h2>}
       
     </div>
   );
--- MesoAmerica/src/assets/Pages/QuizOlmec.jsx
+++ MesoAmerica/src/assets/Pages/QuizOlmec.jsx
@@ -1,26 +1,36 @@
 import QuizComp from "../components/QuizComp.jsx"
 import Header from "../components/Header.jsx"
+import { useState } from "react";
 
 
 function QuizOlmec(){
+  const [score, setScore] = useState(0);
   return(
         <div className="min-h-screen w-full bg-gradient-to-b from-green-200 via-green-400 to-green-700">
       <Header
         title = "Olmec Quiz #1"
       />
       <QuizComp
-        question = "Why are the Olmec civilization often called the “mother culture” of Mesoamerica?"
-        wAns1 = "They were the last civilization in the region"
-        wAns2 = "They only lived in one small village"
-        rAns = "They influenced later Mesoamerican cultures"
+        question1 = "Why are the Olmec civilization often called the “mother culture” of Mesoamerica?"
+        wAns1a = "They were the last civilization in the region"
+        wAns1b = "They only lived in one small village"
+        rAns1 = "They influenced later Mesoamerican cultures"
+
+        question2 = "What was the capital of the Olmec civilization?"
+        wAns2a = "La Venta"
+        wAns2b = "Tres Zapotes"
+        rAns2 = "San Lorenzo"
+
+        question3 = "Why are the Olmec civilization often called the “mother culture” of Mesoamerica?"
+        wAns3a = "They were the last civilization in the region"
+        wAns3b = "They only lived in one small village"
+        rAns3 = "They influenced later Mesoamerican cultures"
+
+        score={score}
+        setScore={setScore}
       />
-      <QuizComp
-        question = "What farming method did the Olmec use to clear land for agriculture?"
-        wAns1 = "Irrigation canals only"
-        wAns2 = "Terrace farming"
-        rAns = "Slash-and-burn farming"
-      />
-      
+     
+      {score !== 0 && <h2>Your score is: {score}/3</h2>}
       
     </div>
   );
