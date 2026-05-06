cc014c4f074452b6c331bd4c18c22b785cff7abe
jasper123234 <jasper.cronin@gmail.com>
2026-05-06 11:15:41 -0700

    YUHYUH FOOTER AND WE DONE I THINK I HOPE

---
+++ MesoAmerica/src/assets/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer className="bg-gradient-to-r from-green-200/95 via-orange-300/95 to-yellow-500/95 border-4 border-yellow-900 text-yellow-950 py-4 text-center font-bold">
+        <p>&copy; 2026 MesoAmerica. All rights reserved. @28.ezzy @51o.jasper @MesoAmerica </p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
