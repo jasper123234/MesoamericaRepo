793beee968394a837d40aa2e4924ad0dd7d83680
EzraZC <ezrazeliecampbell@gmail.com>
2026-05-06 11:10:25 -0700

    PUSHDATBAW

--- MesoAmerica/src/assets/Pages/MayanLearningMod2.jsx
+++ MesoAmerica/src/assets/Pages/MayanLearningMod2.jsx
@@ -17,13 +17,13 @@ function MayanLearningModule2(){
       <LearningModule
         title = "Mayan Diet"
         info = "Their most important crop was corn, being used to make a variety of food and drinks including tortillas, flat cakes, and pies. Their diet was largely vegetarian, consisting of beans, sweet potatoes, tomatoes, squash, avocados, and papayas, although they would eat meat if available. "
-        img1 = "dante.jpg"
+        img1 = {Mayafood}
         
       />
       <LearningModule
        title = "Agricultural Practices"
         info = "The Maya civilization used smart farming methods to grow food in tough environments. They practiced slash-and-burn farming, built raised fields in wet areas, and used terraces on hillsides to prevent erosion. Their main crops were Maize, Beans, and Squash, which helped keep the soil healthy and provided a balanced diet."
-        img1 = "dante.jpg"
+        img1 = {Mayafarm}
       />
     </div>
   );
